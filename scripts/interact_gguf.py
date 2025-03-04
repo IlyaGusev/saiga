44e42d26e7a1535385ec76aa9fb2b9a9844fee8c
Ilya Gusev <phoenixilya@gmail.com>
2025-03-04 15:34:36 +0000

    Interact GGUF script

---
+++ scripts/interact_gguf.py
@@ -0,0 +1,43 @@
+import fire
+from llama_cpp import Llama
+
+SYSTEM_PROMPT = "Ты — Сайга, русскоязычный автоматический ассистент. Ты разговариваешь с людьми и помогаешь им."
+
+
+def interact(
+    model_path,
+    n_ctx=8192,
+    top_k=30,
+    top_p=0.9,
+    temperature=0.6,
+    repeat_penalty=1.1
+):
+    model = Llama(
+        model_path=model_path,
+        n_ctx=n_ctx,
+        n_parts=1,
+        verbose=True,
+    )
+    messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+    while True:
+        user_message = input("User: ")
+        messages.append({"role": "user", "content": user_message})
+        assistant_message = ""
+        for part in model.create_chat_completion(
+            messages,
+            temperature=temperature,
+            top_k=top_k,
+            top_p=top_p,
+            repeat_penalty=repeat_penalty,
+            stream=True,
+        ):
+            delta = part["choices"][0]["delta"]
+            if "content" in delta:
+                print(delta["content"], end="", flush=True)
+                assistant_message += delta["content"]
+        print()
+        messages.append({"role": "assistant", "content": assistant_message})
+
+
+if __name__ == "__main__":
+    fire.Fire(interact)
